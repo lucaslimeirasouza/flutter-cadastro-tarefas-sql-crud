37aa713c610db5e3e888179c66dc18b7923a74df
Lucas Limeira Souza <lucas.limeira@soulisto.com.br>
2024-02-16 16:34:22 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/cadastro_tarefas_sql_crud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cadastro_tarefas_sql_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cadastro_tarefas_sql_crud/views/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
